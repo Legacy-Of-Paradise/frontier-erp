8e632aa6bf394c2868231298a80b1c8ebbb3cc93
BitBoxxxer <143746353+BitBoxxxer@users.noreply.github.com>
2025-05-31 22:02:41 +0500

    clean code for tags

--- Content.Client/_LOP/UI/RText/JobIconRadioTag.cs
+++ Content.Client/_LOP/UI/RText/JobIconRadioTag.cs
@@ -7,87 +7,59 @@ using Robust.Client.UserInterface.Controls;
 using Robust.Client.UserInterface.RichText;
 using Robust.Shared.Configuration;
 using Robust.Shared.Utility;
-using Content.Shared._LOP.LOPCCVars; 
+using Content.Shared._LOP.LOPCCVars;
 
-namespace Content.Client._LOP.UI.RText;
-
-public sealed class RadioIconTag : IMarkupTag
+namespace Content.Client._LOP.UI.RText
 {
-    [Dependency] private readonly IConfigurationManager _cfg = default!;
-    [Dependency] private readonly IResourceCache _cache = default!;
-
-    public string Name => "radicon";
-
-    public bool TryGetControl(MarkupNode node, [NotNullWhen(true)] out Control? control)
+    public sealed class RadioIconTag : IMarkupTag
     {
-        if (_cfg.GetCVar(LOP_CCVars.ChatIconsEnable))
+        [Dependency] private readonly IConfigurationManager _cfg = default!;
+        [Dependency] private readonly IResourceCache _cache = default!;
+
+        public string Name => "radicon";
+
+        public bool TryGetControl(MarkupNode node, [NotNullWhen(true)] out Control? control)
         {
-            if (!node.Attributes.TryGetValue("path", out var rawPath))
+            control = null;
+
+            if (_cfg.GetCVar(LOP_CCVars.ChatIconsEnable))
             {
-                control = null;
+                if (!node.Attributes.TryGetValue("path", out var rawPath))
+                {
+                    return false;
+                }
+
+                if (!node.Attributes.TryGetValue("scale", out var scaleStr) || !scaleStr.TryGetLong(out var scaleValue))
+                {
+                    scaleValue = 1;
+                }
+
+                control = DrawIcon(rawPath.ToString(), scaleValue.Value);
+                return true;
+            }
+            else
+            {
                 return false;
             }
-
-            if (!node.Attributes.TryGetValue("scale", out var scale) || !scale.TryGetLong(out var scaleValue))
-            {
-                scaleValue = 1;
-            }
-
-            control = DrawIcon(rawPath.ToString(), scaleValue.Value);
-        }
-        else
-        {
-            if (!node.Attributes.TryGetValue("text", out var text))
-            {
-                control = null;
-                return false;
-            }
-
-            if (!node.Attributes.TryGetValue("color", out var color))
-            {
-                control = null;
-                return false;
-            }
-
-            control = DrawText(text.ToString(), color.ToString());
         }
 
-        return true;
+        private Control DrawIcon(string path, long scale)
+        {
+            var textureRect = new TextureRect();
+
+            path = ClearString(path);
+
+            textureRect.TexturePath = path;
+            textureRect.TextureScale = new Vector2(scale, scale);
+
+            return textureRect;
+        }
+
+        private static string ClearString(string str)
+        {
+            return str.Replace("=", "")
+                      .Replace(" ", "")
+                      .Replace("\"", "");
+        }
     }
-
-    private Control DrawIcon(string path, long scaleValue)
-    {
-        var texture = new TextureRect();
-
-        path = ClearString(path);
-
-        texture.TexturePath = path;
-        texture.TextureScale = new Vector2(scaleValue, scaleValue);
-
-        return texture;
-    }
-
-    private Control DrawText(string text, string color)
-    {
-        var label = new Label();
-
-        color = ClearString(color);
-        text = ClearString(text);
-
-        label.Text = text;
-        label.FontColorOverride = Color.FromHex(color);
-        label.FontOverride = new VectorFont(_cache.GetResource<FontResource>("/Fonts/NotoSans/NotoSans-Bold.ttf"), 13);
-
-        return label;
-    }
-
-    private static string ClearString(string str)
-    {
-        str = str.Replace("=", "");
-        str = str.Replace(" ", "");
-        str = str.Replace("\"", "");
-
-        return str;
-    }
-
 }
--- Content.Client/_LOP/UI/RText/JobIconTextureTag.cs
+++ Content.Client/_LOP/UI/RText/JobIconTextureTag.cs
@@ -3,39 +3,44 @@ using System.Numerics;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 using Robust.Client.UserInterface.RichText;
-
 using Robust.Shared.Utility;
 
-namespace Content.Client._LOP.UI.RText;
-
-public sealed class TextureTag : IMarkupTag
+namespace Content.Client._LOP.UI.RText
 {
-    public string Name => "tex";
-
-    public bool TryGetControl(MarkupNode node, [NotNullWhen(true)] out Control? control)
+    public sealed class TextureTag : IMarkupTag
     {
-        if (!node.Attributes.TryGetValue("path", out var rawPath))
+        public string Name => "tex";
+
+        public bool TryGetControl(MarkupNode node, [NotNullWhen(true)] out Control? control)
         {
             control = null;
-            return false;
+
+            if (!node.Attributes.TryGetValue("path", out var rawPath))
+            {
+                return false;
+            }
+
+            if (!node.Attributes.TryGetValue("scale", out var scale) || !scale.TryGetLong(out var scaleValue))
+            {
+                scaleValue = 1;
+            }
+
+            var textureRect = new TextureRect();
+
+            var path = SanitizeString(rawPath.ToString());
+
+            textureRect.TexturePath = path;
+            textureRect.TextureScale = new Vector2(scaleValue.Value, scaleValue.Value);
+
+            control = textureRect;
+            return true;
         }
 
-        if (!node.Attributes.TryGetValue("scale", out var scale) || !scale.TryGetLong(out var scaleValue))
+        private static string SanitizeString(string input)
         {
-            scaleValue = 1;
+            return input.Replace("=", "")
+                        .Replace(" ", "")
+                        .Replace("\"", "");
         }
-
-        var texture = new TextureRect();
-
-        var path = rawPath.ToString();
-        path = path.Replace("=", "");
-        path = path.Replace(" ", "");
-        path = path.Replace("\"", "");
-
-        texture.TexturePath = path;
-        texture.TextureScale = new Vector2(scaleValue.Value, scaleValue.Value);
-
-        control = texture;
-        return true;
     }
 }
