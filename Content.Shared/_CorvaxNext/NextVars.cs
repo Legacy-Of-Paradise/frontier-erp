2baf801d9b380679deb758c6e4261942c220f4aa
BitBoxxxer <143746353+BitBoxxxer@users.noreply.github.com>
2025-06-13 14:09:19 +0500

    clean code

--- Content.Shared/_CorvaxNext/NextVars.cs
+++ Content.Shared/_CorvaxNext/NextVars.cs
@@ -6,25 +6,8 @@ namespace Content.Shared._CorvaxNext.NextVars;
 ///     Corvax modules console variables
 /// </summary>
 [CVarDefs]
-// ReSharper disable once InconsistentNaming
 public sealed class NextVars
 {
-    /// <summary>
-    /// Offer item.
-    /// </summary>
-    public static readonly CVarDef<bool> OfferModeIndicatorsPointShow =
-        CVarDef.Create("hud.offer_mode_indicators_point_show", true, CVar.ARCHIVE | CVar.CLIENTONLY);
-
-    /// <summary>
-    /// _CorvaxNext Surgery cvars
-    /// </summary>
-    #region Surgery
-
-    public static readonly CVarDef<bool> CanOperateOnSelf =
-        CVarDef.Create("surgery.can_operate_on_self", false, CVar.SERVERONLY);
-
-    #endregion
-
     /*
      * _CorvaxNext Bind Standing and Laying System
      */
@@ -38,7 +21,4 @@ public sealed class NextVars
     /// </summary>
     public static readonly CVarDef<bool> CrawlUnderTables =
         CVarDef.Create("laying.crawlundertables", true, CVar.REPLICATED);
-
-    // public static readonly CVarDef<bool> OfferModeIndicatorsPointShow =
-    //     CVarDef.Create("hud.offer_mode_indicators_point_show", true, CVar.ARCHIVE | CVar.CLIENTONLY);
 }
